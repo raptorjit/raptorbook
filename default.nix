9d713618a0647a46d2b50f6158c0db803f2be943
Luke Gorrie <luke@snabb.co>
2019-06-01 14:38:04 +0200

    s/raptorjit-patterns/raptorjit-optimization/

--- default.nix
+++ default.nix
@@ -5,14 +5,14 @@ let css = fetchurl {
             sha256 = "089ng30fskkv9j6aqakffcrmgibidg778jfdgjsfk6s1lg14yc49";
           }; in
 
-runCommand "raptorjit-patterns-manual" { buildInputs = [ emacs ]; } ''
-  cp ${./raptorjit-patterns.org} raptorjit-patterns.org
+runCommand "raptorjit-optimization-manual" { buildInputs = [ emacs ]; } ''
+  cp ${./raptorjit-optimization.org} raptorjit-optimization.org
   emacs -Q --batch --eval "
     (progn
       (require 'ox-html)
-      (with-current-buffer (find-file-noselect \"raptorjit-patterns.org\")
+      (with-current-buffer (find-file-noselect \"raptorjit-optimization.org\")
         (org-html-export-to-html)))"
-  install -D raptorjit-patterns.html $out/raptorjit-patterns.html
+  install -D raptorjit-optimization.html $out/raptorjit-optimization.html
   install ${css} $out/org.css
 ''
 
