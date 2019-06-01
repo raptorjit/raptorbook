f8414f800b3a1ec0803ef8b66d31ef6f29e25946
Luke Gorrie <luke@snabb.co>
2019-06-01 14:42:44 +0200

    default.nix: Give derivation a name

--- default.nix
+++ default.nix
@@ -5,14 +5,16 @@ let css = fetchurl {
             sha256 = "089ng30fskkv9j6aqakffcrmgibidg778jfdgjsfk6s1lg14yc49";
           }; in
 
-runCommand "raptorjit-optimization-manual" { buildInputs = [ emacs ]; } ''
-  cp ${./raptorjit-optimization.org} raptorjit-optimization.org
-  emacs -Q --batch --eval "
-    (progn
-      (require 'ox-html)
-      (with-current-buffer (find-file-noselect \"raptorjit-optimization.org\")
-        (org-html-export-to-html)))"
-  install -D raptorjit-optimization.html $out/raptorjit-optimization.html
-  install ${css} $out/org.css
-''
+runCommand "raptorjit-optimization-manual"
+  { name = "raptorbook"; buildInputs = [ emacs ]; }
+  ''
+    cp ${./raptorjit-optimization.org} raptorjit-optimization.org
+    emacs -Q --batch --eval "
+      (progn
+        (require 'ox-html)
+        (with-current-buffer (find-file-noselect \"raptorjit-optimization.org\")
+          (org-html-export-to-html)))"
+    install -D raptorjit-optimization.html $out/raptorjit-optimization.html
+    install ${css} $out/org.css
+  ''
 
