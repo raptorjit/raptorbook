ad1604e85c22b833f27f67e497e0a8e43602de16
Luke Gorrie <luke@snabb.co>
2019-06-02 13:45:47 +0200

    default.nix: Move CSS to inline

--- default.nix
+++ default.nix
@@ -9,8 +9,24 @@ runCommand "raptorjit-optimization-manual"
   { name = "raptorbook"; buildInputs = [ emacs ]; }
   ''
     cp ${./raptorjit-optimization.org} raptorjit-optimization.org
+    cp ${css} org.css
     emacs -Q --batch --eval "
       (progn
+
+	(defun my-org-inline-css-hook (exporter)
+	  (when (eq exporter 'html)
+	    (setq org-html-head-include-default-style nil)
+	    (setq org-html-head (concat
+				 \"<style type=\\\"text/css\\\">\n\"
+				 \"<!--/*--><![CDATA[/*><!--*/\n\"
+				 (with-temp-buffer
+				   (insert-file-contents \"org.css\")
+				   (buffer-string))
+				 \"/*]]>*/-->\n\"
+				 \"</style>\n\"))))
+
+	(add-hook 'org-export-before-processing-hook 'my-org-inline-css-hook)
+
         (message \"Loading ox-html\")
         (require 'ox-html)
 	(message \"Opening org file\")
@@ -19,9 +35,12 @@ runCommand "raptorjit-optimization-manual"
           (org-html-export-to-html)
 	  (message \"Done\")))"
     install -D raptorjit-optimization.html $out/raptorjit-optimization.html
-    install ${css} $out/org.css
+    #install ${css} $out/org.css
 
     mkdir $out/nix-support
     echo "file HTML $out/raptorjit-optimization.html" >> $out/nix-support/hydra-build-products
   ''
+
+
+
 
