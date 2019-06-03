2663aace1f7ee97efc55fdb74eadf099ad077564
Luke Gorrie <luke@snabb.co>
2019-06-03 12:42:39 +0200

    default.nix: Enable more emacs/org features

--- default.nix
+++ default.nix
@@ -1,17 +1,25 @@
 with import <nixpkgs> {};
 
-let css = fetchurl {
-            url = "https://raw.githubusercontent.com/gongzhitaao/orgcss/b1c9f8975dc2346b247910c861f0f4a193d63c68/docs/org.css";
-            sha256 = "08la1404s339nvcpnspz2pf29c3bnp5lsmvfs5gdqp7sfzag9n07";
-          }; in
+let
+  css = fetchurl {
+          url = "https://raw.githubusercontent.com/gongzhitaao/orgcss/b1c9f8975dc2346b247910c861f0f4a193d63c68/docs/org.css";
+          sha256 = "08la1404s339nvcpnspz2pf29c3bnp5lsmvfs5gdqp7sfzag9n07";
+        };
+  myEmacs = emacsWithPackages (epkgs: (with epkgs;
+              [ use-package htmlize lua-mode org-ref ]));
+in
 
 runCommand "raptorjit-optimization-manual"
-  { name = "raptorbook"; buildInputs = [ emacs ]; }
+  { name = "raptorbook"; buildInputs = [ myEmacs ]; }
   ''
     cp ${./raptorjit-optimization.org} raptorjit-optimization.org
     cp ${css} org.css
     emacs -Q --batch --eval "
       (progn
+
+        (setq debug-on-error t)
+        (setq org-src-fontify-natively t)
+        (require 'lua-mode)
 
 	(defun my-org-inline-css-hook (exporter)
 	  (when (eq exporter 'html)
@@ -27,8 +35,15 @@ runCommand "raptorjit-optimization-manual"
 
 	(add-hook 'org-export-before-processing-hook 'my-org-inline-css-hook)
 
+        (require 'use-package)
+        (package-initialize 'noactivate)
+        (use-package htmlize)
+        (use-package lua-mode)
+        (use-package org-ref)
         (message \"Loading ox-html\")
         (require 'ox-html)
+        (message \"Loading htmlize\")
+        (require 'htmlize)
 	(message \"Opening org file\")
         (with-current-buffer (find-file-noselect \"raptorjit-optimization.org\")
 	  (message \"Exporting HTML\")
